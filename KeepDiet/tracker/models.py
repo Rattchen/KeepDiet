988e7a8d70e02078476f9d7266e0f5bc95e7f59e
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-01-17 09:51:23 +0100

    Create 'Meal' model

--- KeepDiet/tracker/models.py
+++ KeepDiet/tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    """ Holds info pertaining to a single meal"""
+
+    class MealType(models.TextChoices):
+        BREAKFAST = 'breakfast', 'Breakfast'
+        LUNCH = 'lunch', 'Lunch'
+        DINNER = 'dinner', 'Dinner'
+        DESSERT = 'dessert', 'Dessert'
+        SUPPER = 'supper', 'Supper'
+        SNACK = 'snack', 'Snack'
+        DRINK = 'drink', 'Drink'
+        OTHER = 'other', 'Other'
+
+    date = models.DateField()
+    time = models.TimeField()
+    category = models.CharField(max_length=25, choices=MealType.choices)
+    name = models.CharField(max_length=250)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=25)
+    calories = models.IntegerField()
