3412b58fdc90c6de186ddb28ffd4bf97fb3a1f7e
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-01-17 12:26:35 +0100

    Create a simple DaySummary view

--- KeepDiet/tracker/views.py
+++ KeepDiet/tracker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import DaySummary
 
 def index(request):
-    return HttpResponse("Hello from 'tracker' app!")
+    day = DaySummary.objects.get(id=1)
+    meals = day.meals.all()
+    return render(request, 'tracker/index.html', context={'day':day, 'meals':meals})
