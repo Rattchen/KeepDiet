66eab19f271373cc8505b5b1970c452b490e00bd
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-01-17 13:04:44 +0100

    Change the DaySummary view to class based

--- KeepDiet/tracker/urls.py
+++ KeepDiet/tracker/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('day/<int:pk>/', views.DayDetailView.as_view(), name='day-detail')
 ]
--- KeepDiet/tracker/views.py
+++ KeepDiet/tracker/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView
 from .models import DaySummary
 
-def index(request):
-    day = DaySummary.objects.get(id=1)
-    meals = day.meals.all()
-    return render(request, 'tracker/index.html', context={'day':day, 'meals':meals})
+class DayDetailView(DetailView):
+    model = DaySummary
+    template_name = 'tracker/index.html'
+    context_object_name = 'day'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = self.object.meals.all()
+        return context
