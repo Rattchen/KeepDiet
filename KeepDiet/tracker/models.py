f12e830908b1e599de67cd08732867ca022e5140
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-03-14 01:19:50 +0100

    Add model for keeping track of body measurements

--- KeepDiet/tracker/models.py
+++ KeepDiet/tracker/models.py
@@ -7,6 +7,19 @@ class TrackerProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s profile"
 
+class Measurements(models.Model):
+    """ Holds body measurements to keep track of the progress"""
+    
+    unit = models.CharField(max_length=25, null=True, blank=True)
+    arm = models.FloatField(null=True, blank=True)
+    chest = models.FloatField(null=True, blank=True)
+    waist = models.FloatField(null=True, blank=True)
+    stomach = models.FloatField(null=True, blank=True)
+    hips = models.FloatField(null=True, blank=True)
+    thighs = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Measurements for {self.day}"
 
 class DaySummary(models.Model):
     """ Holds additional info about the whole day, like activity or weight"""
@@ -14,6 +27,7 @@ class DaySummary(models.Model):
 
     user = models.ForeignKey(TrackerProfile, on_delete=models.CASCADE)
     weight = models.FloatField(null=True, blank=True)
+    measurements = models.ForeignKey(Measurements, on_delete=models.CASCADE, related_name="day", null=True, blank=True)
     was_active = models.BooleanField()
     activity_name = models.CharField(max_length=250, null=True, blank=True)
     activity_quantity = models.FloatField(null=True, blank=True)
@@ -22,6 +36,9 @@ class DaySummary(models.Model):
     @property
     def total_calories(self):
         return sum(meal.calories or 0 for meal in self.meals.all())
+
+    def __str__(self):
+        return f"Day summary for {self.user.user.username}"
 
 class Meal(models.Model):
     """ Holds info pertaining to a single meal"""
