58380d6cdcb640fd1e98da1e724ce49cc9c41751
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-04-12 16:23:11 +0200

    Add Vantry to the project
    Vantry is an independent app so it has its own repository. It's an app designed for keeping track of food in one's household - both the amount of it, as well as the expiry date.

--- KeepDiet/KeepDiet/urls.py
+++ KeepDiet/KeepDiet/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include('tracker.urls')),
+    path("vantry/", include('vantry.urls')),
     path("user/", include('accounts.urls')),
     path("admin/", admin.site.urls),
 ]
